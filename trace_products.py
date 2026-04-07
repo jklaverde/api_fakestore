70068bcb2e46b4c131d04b6c67dae2866bfec3fa
jklaverde <juank.laverde@gmail.com>
2026-04-07 15:25:06 +0200

    Extend the number of products to be reviewed through different scrapped files

--- trace_products.py
+++ trace_products.py
@@ -1,7 +1,19 @@
 import pandas as pd, glob
 
+print ("*****************************")
+print (" ")
+print('This snipped code shows the changes per product on the different csv files (per scrapped file)')
+print (" ")
+print ("*****************************")
+
+
 frames = [pd.read_csv(f) for f in sorted(glob.glob("scraped_data/*.csv"))]
 df = pd.concat(frames, ignore_index=True)
 
 # Show price changes for product 1
 print(df[df["id"] == 1][["scraped_at", "price", "title"]])
+
+for product_id in range(20):
+    print(df[df["id"] == product_id][["scraped_at", "price", "title", "rating_count"]])
+    print ("------------------------")
+
